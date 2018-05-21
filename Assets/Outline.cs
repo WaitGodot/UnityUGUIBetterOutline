3e61a0c2d559aa5e839f9e5cb792f71488b54e4b
randy <randy@cosyjoy.com>
2018-05-21 14:56:37 +0800

    better outline

---
+++ Assets/Outline.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class Outline : BaseMeshEffect
+{
+    Text text;
+
+    [Range(1, 10)]
+    public float OutlineWidth = 1;
+    [Range(1,10)]
+    public int OutlineSoltness = 5;
+    public Color32 OutlineColor = new Color32(0,0,0,255);
+
+    protected override void Start()
+    {
+        text = GetComponent<Text>();
+        if (text)
+        {
+            text.material = new UnityEngine.Material(Shader.Find("Unlit/outline"));
+        }
+        UpdateMaterail();
+    }
+
+    protected void UpdateMaterail()
+    {
+        if (text == null || text.material == null)
+        {
+            return;
+        }
+
+        var mat = text.material;
+        float w = OutlineWidth * text.fontSize / 24.0f;
+        w = w < 1 ? 1 : w;
+        w = w > 10 ? 10 : w;
+        mat.SetFloat("_OutlineWidth", w);// * text.fontSize / 24.0f * 2);
+        mat.SetInt("_OutlineSoftness", OutlineSoltness);
+        mat.SetColor("_OutlineColor", OutlineColor);
+    }
+
+#if UNITY_EDITOR
+    float ow = 0;
+    float os = 0;
+    Color32 oc = new Color32(0, 0, 0, 0);
+    protected void Update()
+    {
+        if (ow != OutlineWidth ||
+            os != OutlineSoltness ||
+            oc.Equals(OutlineColor))
+        {
+            UpdateMaterail();
+        }
+        ow = OutlineWidth;
+        os = OutlineSoltness;
+        oc = OutlineColor;
+    }
+#endif
+
+    public override void ModifyMesh(VertexHelper vh)
+    {
+        if (!IsActive())
+            return;
+        var verts = new List<UIVertex>();
+        vh.GetUIVertexStream(verts);
+
+        UIVertex vt;
+        float s = 0.1f;
+        for (int i = 0; i < verts.Count / 6; ++i)
+        {
+            int j = i * 6;
+            Vector2 uv1 = Vector2.zero, uv2 = Vector2.zero;
+
+            Vector2 uv11 = verts[j].uv0;
+            Vector2 uv21 = verts[j + 2].uv0;
+            Vector3 p1 = verts[j].position;
+            Vector3 p2 = verts[j + 2].position;
+
+            uv1.x = Mathf.Min(uv11.x, uv21.x);
+            uv1.y = Mathf.Min(uv11.y, uv21.y);
+            uv2.x = Mathf.Max(uv11.x, uv21.x);
+            uv2.y = Mathf.Max(uv11.y, uv21.y);
+
+            float uvw_x = uv2.x - uv1.x;
+            float uvw_y = uv2.y - uv1.y;
+            float pw_x = Mathf.Abs(p1.x - p2.x);
+            float pw_y = Mathf.Abs(p1.y - p2.y);
+
+            for (int k = 0; k < 6; k ++)
+            {
+                vt = verts[j + k];
+                switch(k)
+                {
+                    case 0:
+                    case 5:
+                        vt.position.x -= pw_x * s;
+                        vt.position.y += pw_y * s;
+                        break;
+                    case 1:
+                        vt.position.x += pw_x * s;
+                        vt.position.y += pw_y * s;
+                        break;
+                    case 2:
+                    case 3:
+                        vt.position.x += pw_x * s;
+                        vt.position.y -= pw_y * s;
+                        break;
+                    case 4:
+                        vt.position.x -= pw_x * s;
+                        vt.position.y -= pw_y * s;
+                        break;
+                }  
+
+                if (vt.uv0.x == uv2.x)
+                {
+                    vt.uv0.x += uvw_x * s;
+                }else
+                {
+                    vt.uv0.x -= uvw_x * s;
+                }
+
+                if (vt.uv0.y == uv2.y)
+                {
+                    vt.uv0.y += uvw_y * s;
+                }
+                else
+                {
+                    vt.uv0.y -= uvw_y * s;
+                }
+                
+                vt.uv1 = uv1;
+                vt.uv2 = uv2;
+
+                verts[j + k] = vt;
+            }
+        }
+        vh.Clear();
+        vh.AddUIVertexTriangleStream(verts);
+    }
+}
---
+++ Assets/outline.shader
@@ -0,0 +1,133 @@
+﻿Shader "Unlit/outline"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		
+		_OutlineColor("Outline Color", Color) = (0, 0, 0, 1)
+		_OutlineWidth("Outline Width", Range(1, 10)) = 1
+		_OutlineSoftness("Outline Softness", Range(1, 10)) = 4
+
+		_StencilComp("Stencil Comparison", Float) = 8
+		_Stencil("Stencil ID", Float) = 0
+		_StencilOp("Stencil Operation", Float) = 0
+		_StencilWriteMask("Stencil Write Mask", Float) = 255
+		_StencilReadMask("Stencil Read Mask", Float) = 255
+		_ColorMask("Color Mask", Float) = 15
+	}
+	SubShader
+	{
+		Tags { "Queue" = "Transparent" "RenderType" = "Transparent" }
+		LOD 100
+		Fog{ Mode Off }
+		Cull Off
+		ZWrite Off
+		Lighting Off
+		ColorMask[_ColorMask]
+
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+
+			#include "UnityCG.cginc"
+
+			uniform float4	_OutlineColor;
+			uniform float	_OutlineWidth;
+			uniform int	_OutlineSoftness;
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float4 color : COLOR;
+				float2 uv : TEXCOORD0;
+				float2 uv1 : TEXCOORD1;
+				float2 uv2 : TEXCOORD2;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float2 uv1 : TEXCOORD1;
+				float2 uv2 : TEXCOORD2;
+				float4 vertex : SV_POSITION;
+				float4 color : COLOR;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			uniform half4 _MainTex_TexelSize;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.uv1 = TRANSFORM_TEX(v.uv1, _MainTex);
+				o.uv2 = TRANSFORM_TEX(v.uv2, _MainTex);
+				o.color = v.color;
+
+				return o;
+			}
+
+			float4 frag (v2f i) : SV_Target
+			{
+				float4 col = tex2D(_MainTex, i.uv);
+				float4 outlinecolor = _OutlineColor; 
+
+				float2 uv = i.uv;
+				float2 uv1 = i.uv1;
+				float2 uv2 = i.uv2;
+
+				float4 ot = float4(0,0,0,0);
+				float x = 0.0;
+				float y = 0.0;
+
+				float4 color = i.color;
+				float lx = uv1.x;
+				float rx = uv2.x;
+				float dy = uv1.y;
+				float ty = uv2.y;
+
+				float radius = _OutlineWidth / 15.0;
+
+				for (int i = 1; i < _OutlineSoftness + 1; i++)
+				{
+					x = i * radius; y = i * radius;
+
+					uv1 = uv + _MainTex_TexelSize.xy * half2(x, 0);
+					ot += tex2D(_MainTex, float2(clamp(uv1.x, lx, rx), clamp(uv1.y, dy, ty)));
+					uv1 = uv + _MainTex_TexelSize.xy * half2(x, y);
+					ot += tex2D(_MainTex, float2(clamp(uv1.x, lx, rx), clamp(uv1.y, dy, ty)));
+					uv1 = uv + _MainTex_TexelSize.xy * half2(0, y);
+					ot += tex2D(_MainTex, float2(clamp(uv1.x, lx, rx), clamp(uv1.y, dy, ty)));
+					uv1 = uv + _MainTex_TexelSize.xy * half2(-x, y);
+					ot += tex2D(_MainTex, float2(clamp(uv1.x, lx, rx), clamp(uv1.y, dy, ty)));
+					uv1 = uv + _MainTex_TexelSize.xy * half2(-x, 0);
+					ot += tex2D(_MainTex, float2(clamp(uv1.x, lx, rx), clamp(uv1.y, dy, ty)));
+					uv1 = uv + _MainTex_TexelSize.xy * half2(-x, -y);
+					ot += tex2D(_MainTex, float2(clamp(uv1.x, lx, rx), clamp(uv1.y, dy, ty)));
+					uv1 = uv + _MainTex_TexelSize.xy * half2(0, -y);
+					ot += tex2D(_MainTex, float2(clamp(uv1.x, lx, rx), clamp(uv1.y, dy, ty)));
+					uv1 = uv + _MainTex_TexelSize.xy * half2(x, -y);
+					ot += tex2D(_MainTex, float2(clamp(uv1.x, lx, rx), clamp(uv1.y, dy, ty)));
+				}
+
+				float a = step(lx, uv.x) * step(uv.x, rx) * step(dy, uv.y) * step(uv.y, ty);
+				// float b = (1 - step(ot.a, 0)) * (step(col.a, 1) + 1 - a);
+
+				outlinecolor.a = outlinecolor.a * ot.a;
+				color.a *= col.a;
+				color = color * a;
+				outlinecolor = lerp(outlinecolor, color, col.a) + color * color.a;
+
+				return outlinecolor;// lerp(color, outlinecolor, b);
+			}
+			ENDCG
+		}
+	}
+}
